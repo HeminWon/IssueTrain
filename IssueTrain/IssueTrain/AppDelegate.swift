daa5141898698b6280734a2d03ebd15ff76639a3
王明海 <wangmignhai@didichuxing.com>
2018-07-21 20:40:47 +0800

    add IssueTrain

---
+++ IssueTrain/IssueTrain/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IssueTrain
+//
+//  Created by HeminWon on 2018/7/21.
+//  Copyright © 2018 HeminWon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IssueTrain/IssueTrain/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IssueTrain
+//
+//  Created by HeminWon on 2018/7/21.
+//  Copyright © 2018 HeminWon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
